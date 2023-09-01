dfcfb6a09a63945914968683f287ef4171fcc14b
Leonardo Hernández <leonardoahl1507@gmail.com>
2023-09-01 10:50:59 -0600

    no acabe >:c

--- filtro.js
+++ filtro.js
@@ -1,5 +1,6 @@
 // Tenemos un li de productos
-
+// changed all declarations of var to const 
+// gave readable names to variables 
 const productos = [
   {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
   {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
@@ -8,56 +9,61 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const productItem = document.getElementById("lista-de-productos") // changed to getElementById
+const filterInput = document.getElementById("filterInput"); // changed to getElementById
+const botonDeFiltro = document.getElementById("filterBtn"); // changed to getElementById
+console.log(productItem);
+console.log(filterInput);
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
-
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+function displayProductos(productos){ //inserted the for into a function named display  products
+  for (let i = 0; i < productos.length; i++) {
+    const div = document.createElement("div")
+    div.classList.add("producto")
   
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
-
-  d.appendChild(ti)
-  d.appendChild(imagen)
-
-  li.appendChild(d)
+    const title = document.createElement("p")
+    title.classList.add("titulo")
+    title.textContent = productos[i].nombre
+    
+    const imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
+  
+    div.appendChild(title)
+    div.appendChild(imagen)
+  
+    productItem.appendChild(div)
+  }
 }
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
-
-botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
+botonDeFiltro.onclick = () => { // made it an arrow function bc it looks better for me :v
+  while (productItem.firstChild) {
+    productItem.removeChild(productItem.firstChild);
   }
 
-  const texto = $i.value;
+  const texto = filterInput.value;
   console.log(texto);
   const productosFiltrados = filtrado(productos, texto );
 
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
+    const div = document.createElement("div")
+    div.classList.add("producto")
   
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
+    const title = document.createElement("p")
+    title.classList.add("titulo")
+    title.textContent = productosFiltrados[i].nombre
     
-    var imagen = document.createElement("img");
+    const imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
   
-    d.appendChild(ti)
-    d.appendChild(imagen)
+    div.appendChild(title)
+    div.appendChild(imagen)
   
-    li.appendChild(d)
+    productItem.appendChild(div)
   }
 }
 
-const filtrado = (productos = [], texto) => {
-  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+const filtrado = (productos = [], texto) => { //made it possible to search specific name but only works if typed correctly :c
+  console.log(texto)
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto) || item.nombre.includes(texto)); 
 }  
+// ME NEED TIME >:(((((((
+displayProductos(productos)
